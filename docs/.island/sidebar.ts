a4c9fd617716818b6382fbdd13b9fd26b073a3ec
plasticine <plasticine@plasticine-ubuntu-22.04>
2022-11-25 23:10:56 +0800

    refactor(docs): sidebar structure

--- docs/.island/sidebar.ts
+++ docs/.island/sidebar.ts
@@ -1,17 +1,28 @@
 import { DefaultTheme } from 'islandjs'
 
+const generateBasics = () => {
+  /** @description 生成基础篇文章链接 */
+  const generateLink = (name: string) => `/guide/basics/${name}`
+
+  const basics: DefaultTheme.SidebarGroup = {
+    text: '基础篇',
+    items: [
+      {
+        text: '认识 JSX',
+        link: generateLink('jsx'),
+      },
+      {
+        text: '起源 Component',
+        link: generateLink('component'),
+      },
+    ],
+  }
+
+  return basics
+}
+
 const sidebar: DefaultTheme.Sidebar = {
-  '/guide/': [
-    {
-      text: 'Guide',
-      items: [
-        {
-          text: '认识 JSX',
-          link: '/guide/jsx',
-        },
-      ],
-    },
-  ],
+  '/guide/': [generateBasics()],
 }
 
 export { sidebar }
--- docs/index.md
+++ docs/index.md
@@ -8,7 +8,7 @@ hero:
   actions:
     - theme: brand
       text: Get Started
-      link: /guide/jsx
+      link: /guide/basics/jsx
     - theme: alt
       text: View on GitHub
       link: https://github.com/Plasticine-Yang/react-advanced-guide
