5b79ea5884f84be0bd7e758c5a77272b0c293278
plasticine <plasticine@plasticine-ubuntu-22.04>
2022-11-26 11:10:59 +0800

    feat: use astro

--- build/constants.ts
+++
@@ -1,6 +0,0 @@
-import { resolve } from 'path'
-import { fileURLToPath } from 'url'
-
-const __dirname = fileURLToPath(new URL('.', import.meta.url))
-
-export const PROJECT_ROOT = resolve(__dirname, '..')
--- src/demos/jsx/babel-jsx.ts
+++
@@ -1,7 +0,0 @@
-import { transformSync } from '@babel/core'
-
-const res = transformSync('<div>jsx</div>', {
-  presets: [['@babel/preset-react', { runtime: 'automatic' }]],
-})!
-
-console.log(res.code)
