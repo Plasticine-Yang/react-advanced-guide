35676815261868ef8ded62e9a2d5dd681c42d55c
plasticine9750 <975036719@qq.com>
2022-12-12 12:47:15 +0800

    refactor: 重构侧边栏配置

--- docs/.island/sidebar.ts
+++ docs/.island/sidebar.ts
@@ -1,52 +1,83 @@
 import { DefaultTheme } from 'islandjs'
 
-const generateBasics = () => {
-  /** @description 生成基础篇文章链接 */
-  const generateLink = (name: string) => `/guide/basics/${name}`
+/** @description 基础篇 */
+const basicSidebarGroupConfig: SidebarGroupConfig = {
+  path: '/guide/basics/',
+  displayName: '基础篇',
+  items: [
+    {
+      text: 'jsx',
+      link: 'jsx',
+    },
+    {
+      text: 'component',
+      link: 'component',
+    },
+    {
+      text: 'state',
+      link: 'state',
+    },
+    {
+      text: 'props',
+      link: 'props',
+    },
+    {
+      text: 'life cycle',
+      link: 'life-cycle',
+    },
+    {
+      text: 'ref',
+      link: 'ref',
+    },
+    {
+      text: 'context',
+      link: 'context',
+    },
+    {
+      text: 'HOC',
+      link: 'HOC',
+    },
+  ],
+}
 
-  const basics: DefaultTheme.SidebarGroup = {
-    text: '基础篇',
-    items: [
-      {
-        text: 'jsx',
-        link: generateLink('jsx'),
-      },
-      {
-        text: 'component',
-        link: generateLink('component'),
-      },
-      {
-        text: 'state',
-        link: generateLink('state'),
-      },
-      {
-        text: 'props',
-        link: generateLink('props'),
-      },
-      {
-        text: 'life cycle',
-        link: generateLink('life-cycle'),
-      },
-      {
-        text: 'ref',
-        link: generateLink('ref'),
-      },
-      {
-        text: 'context',
-        link: generateLink('context'),
-      },
-      {
-        text: 'HOC',
-        link: generateLink('HOC'),
-      },
-    ],
+/** @description 优化篇 */
+const optimizationSidebarGroupConfig: SidebarGroupConfig = {
+  displayName: '优化篇',
+  path: '/guide/optimization/',
+  items: [{ text: '渲染控制', link: 'render-control' }],
+}
+
+interface SidebarGroupConfig {
+  /** @description 分类文档存储路径 */
+  path: string
+
+  /** @description 分类展示时的名字 */
+  displayName: string
+
+  /** @description 分类下的文档配置 */
+  items: DefaultTheme.SidebarItem[]
+}
+const generateSidebarGroup = (config: SidebarGroupConfig) => {
+  const { path, displayName, items } = config
+  const generateLink = (name: string) => `${path}${name}`
+
+  const sidebarGroup: DefaultTheme.SidebarGroup = {
+    text: displayName,
+
+    items: items.map((item) => ({
+      text: item.text,
+      link: generateLink(item.link ?? 'not-found'),
+    })),
   }
 
-  return basics
+  return sidebarGroup
 }
 
 const sidebar: DefaultTheme.Sidebar = {
-  '/guide/': [generateBasics()],
+  '/guide/': [
+    generateSidebarGroup(basicSidebarGroupConfig),
+    generateSidebarGroup(optimizationSidebarGroupConfig),
+  ],
 }
 
 export { sidebar }
